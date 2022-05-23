7e16904f9468cb4e85091a562663f95535fc33a5
Aleksandra Mysiak <al.mysiak@gmail.com>
2022-05-23 12:32:50 +0200

    improved evaluation

--- evaluate.py
+++ evaluate.py
@@ -2,8 +2,9 @@ def mean_reciprocal_rank(y_pred, y_gt) -> float:
     ret = 0
     for pred, gt in zip(y_pred, y_gt):
         for i, s in enumerate(pred, 1):
-            if s == gt:
+            # print(i, s)
+            if s.strip().lower() == gt.strip().lower():
                 ret += 1 / i
                 continue
-
+                
     return ret / len(y_gt)
