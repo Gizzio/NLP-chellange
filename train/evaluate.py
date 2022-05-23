c0e4be345b318edcab1e7b55a83c9decd08357d9
Aleksandra Mysiak <al.mysiak@gmail.com>
2022-05-23 11:34:53 +0200

    evaluation function

---
+++ train/evaluate.py
@@ -0,0 +1,9 @@
+def mrr(y_pred, y_gt):
+    ret = 0
+    for pred, gt in zip(y_pred, y_gt):
+        for i, s in enumerate(pred, 1):
+            if s == gt:
+                ret += 1/i
+                continue
+
+    return ret / len(y_gt)
