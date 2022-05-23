60fa82db3e409a4a51932869134aed9d352540a7
Aleksandra Mysiak <al.mysiak@gmail.com>
2022-05-23 12:36:11 +0200

    typed evaluation

--- evaluate.py
+++ evaluate.py
@@ -1,4 +1,7 @@
-def mean_reciprocal_rank(y_pred, y_gt) -> float:
+from typing import Iterable
+
+
+def mean_reciprocal_rank(y_pred: Iterable[Iterable[str]], y_gt: Iterable[str]) -> float:
     ret = 0
     for pred, gt in zip(y_pred, y_gt):
         for i, s in enumerate(pred, 1):
