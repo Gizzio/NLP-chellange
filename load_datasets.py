fe9ffb71752023b9798fe378ca562acccdecd440
Emilia Wiśnios <57295153+emiliawisnios@users.noreply.github.com>
2022-05-23 12:03:11 +0200

    Update load_datasets.py

--- load_datasets.py
+++ load_datasets.py
@@ -1,7 +1,16 @@
 from load_data import load
 
-def load_datasets():
+
+def load_train():
     X_train, X_train_candidates, Y_train = load('train/in.tsv', 'train/expected.tsv')
+    return X_train, X_train_candidates, Y_train
+
+
+def load_dev():
     X_dev, X_dev_candidates, Y_dev = load('dev-0/in.tsv', 'dev-0/expected.tsv')
+    return X_dev, X_dev_candidates, Y_dev
+    
+    
+def load_test():
     X_test, X_test_candidates, _ = load('test-A/in.tsv', 'test-A/out.tsv')
-    return X_train, X_train_candidates, Y_train, X_dev, X_dev_candidates, Y_dev, X_test, X_test_candidates
+    return X_test, X_test_candidates
