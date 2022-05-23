e1f2ef553c40468f953812b9e755889342e95d6b
Stanisław Giziński <s.gizinski84@gmail.com>
2022-05-23 11:55:51 +0200

    model

--- model.py
+++ model.py
@@ -2,11 +2,19 @@ from transformers import AutoTokenizer, AutoModel
 from sklearn.metrics import pairwise
 from sentence_transformers import SentenceTransformer
 from typing import List
-
+import numpy as np
 
 model = SentenceTransformer("Voicelab/sbert-large-cased-pl")
 
 
 def get_prediction(X: List[str], X_cadidates: List[List[str]]) -> List[List[str]]:
     X_embeddings = model.encode(X)
-    X_cadidates_embeddings = model.encode(X_cadidates)
+    results = []
+    for x_cand in X_cadidates:
+        X_cadidates_embeddings = model.encode(x_cand)
+        cosine_similarities = pairwise.cosine_similarity(X_embeddings, X_cadidates_embeddings)
+        print(cosine_similarities)
+        order = np.argsort(cosine_similarities)
+        sorted_result = x_cand[order]
+        results.append(sorted_result)
+    return results
